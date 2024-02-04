14f66231f9d17249a6450ce5a3e95d92ef7d7acb
Vinicius Hirschle <viniciustht@gmail.com>
2024-02-04 15:56:57 -0300

    Setup project

---
+++ src/idle_slayer_automation/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Vinicius Hirschle <viniciustht@gmail.com>
+#
+# SPDX-License-Identifier: MIT
---
+++ src/idle_slayer_automation/cli.py
@@ -0,0 +1,8 @@
+
+import typer
+
+def main():
+    typer.echo("Hello, world!")
+
+if __name__ == "__main__":
+    typer.run(main)
